39307d05d966a81257413e29acd7cc4d3e933c96
Davi Xavier Buzato <davixbuzato@gmail.com>
2022-08-18 16:33:10 -0300

    Fiz Generate

--- Services/CpfService.cs
+++ Services/CpfService.cs
@@ -2,9 +2,9 @@ public class CpfService
 {
     public CpfService(int seed)
     {
-        this.rand = new Random(seed);
+        this.rnd = new Random(seed);
     }
-    Random rand;
+    Random rnd;
 
     public bool Validate(string cpf)
     {
@@ -13,12 +13,32 @@ public class CpfService
 
     public string Generate()
     {
-        throw new NotImplementedException();
+        string semente = rnd.Next(100000000, 999999999).ToString();
+        Console.WriteLine(semente);
+
+        string verif = getValidationDigits(semente);
+        string resuta = semente + verif;
+
+        return resuta;
     }
 
     // detalhes em: https://www.calculadorafacil.com.br/computacao/validar-cpf
     private string getValidationDigits(string cpf9digits)
     {
-        throw new NotImplementedException();
+        int digit0 = 0, digit1 = 0;
+        for (int i = 0; i < cpf9digits.Length; i++)
+        {
+            var digit = cpf9digits[i] - '0';
+            digit0 += digit * (i + 1);
+            digit1 += digit * i;
+        }
+        digit0 %= 11;
+        digit0 %= 10;
+        
+        digit1 += 9 * digit0;
+        digit1 %= 11;
+        digit1 %= 10;
+
+        return digit0.ToString() + digit1.ToString();
     }
 }
